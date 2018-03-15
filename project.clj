9fe883e1cb6f1ea50696b3238659749d3aba8ae8
Isaac Scheinfeld <ischeinfeld@outlook.com>
2018-03-15 08:26:00 -0700

    Updated project.clj with description

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
 (defproject hdvecs "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Computing with High Dimensional Vectors"
+  :url "https://github.com/ischeinfeld/hdvecs"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [net.mikera/core.matrix "0.62.0"]
                  [net.mikera/vectorz-clj "0.47.0"]
